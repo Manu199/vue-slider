2345058035e58a13e65163fecdf8dcd268406b03
Manu199 <manu199gh@gmail.com>
2023-09-28 15:20:20 +0200

    [add] buttons slider

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 
 const { createApp } = Vue;
+
+createApp({
+
+  data(){
+    return{
+    images:[
+      'img/01.webp',
+      'img/02.webp',
+      'img/03.webp',
+      'img/04.webp',
+      'img/05.webp'
+    ],
+    counter: 0,
+    description:{ 
+
+
+    }
+  }
+},
+methods: {
+  nextPrev(isNext){
+
+
+    isNext ? this.counter++ : this.counter--;
+  }
+}
+
+}).mount('#app');
